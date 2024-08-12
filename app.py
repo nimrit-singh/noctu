b5bcb0b08c07c13667eb1b4fc6c5f6506f2a786c
Nmait2111 <namitjain2111@gmail.com>
2024-08-12 19:52:38 +0530

    added flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/blogs")
+def allBLog():
+    return render_template("blog_list.html")
+
+@app.route("/blog/:id")
+def blog(id):
+    return render_template("blog.html")
+
+@app.route("/test-blog")
+def t():
+    return render_template("blog.html")
+app.run(debug=True)
